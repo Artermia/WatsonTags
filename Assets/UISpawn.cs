2e4c7fba65f9c5550dfb861f45db446082769730
Austin Trieu <austrieu@umich.edu>
2017-02-16 13:00:44 -0500

    Almost complete UI

---
+++ Assets/UISpawn.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISpawn : MonoBehaviour {
+
+    public GameObject patientUI;
+    public GameObject watsonUI;
+    public GameObject notepadUI;
+    public GameObject alertsUI;
+    public GameObject cameraUI;
+    public GameObject qrReader;
+    public GameObject ARCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void destroyUI()
+    {
+        GameObject[] UIobjects = GameObject.FindGameObjectsWithTag("UserInterface");
+        foreach(GameObject destroyee in UIobjects)
+        {
+            Destroy(destroyee);
+        }
+    }
+    public void spawnQRReader()
+    {
+        destroyUI();
+        Instantiate(qrReader);
+        Instantiate(ARCamera);
+    }
+
+    public void spawnPatientUI()
+    {
+        destroyUI();
+        Instantiate(patientUI);
+    }
+
+    public void spawnWatsonUI()
+    {
+        destroyUI();
+        Instantiate(watsonUI);
+    }
+
+    public void spawnNotepadUI()
+    {
+        destroyUI();
+        Instantiate(notepadUI);
+    }
+
+    public void spawnAlertsUI()
+    {
+        destroyUI();
+        Instantiate(alertsUI);
+    }
+
+    public void spawnCameraUI()
+    {
+        destroyUI();
+        Instantiate(cameraUI);
+    }
+}
---
+++ Assets/VuforiaScanner.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+using Vuforia;
+
+using System.Threading;
+
+using ZXing;
+using ZXing.QrCode;
+using ZXing.Common;
+
+
+[AddComponentMenu("System/VuforiaScanner")]
+public class VuforiaScanner : MonoBehaviour
+{    
+	private bool cameraInitialized;
+    public UISpawn mainMenu;
+    public GameObject QRcamera;
+
+	private BarcodeReader barCodeReader;
+
+	void Start()
+	{        
+		barCodeReader = new BarcodeReader();
+		StartCoroutine(InitializeCamera());
+	}
+
+	private IEnumerator InitializeCamera()
+	{
+		// Waiting a little seem to avoid the Vuforia's crashes.
+		yield return new WaitForSeconds(1.25f);
+
+        //var isFrameFormatSet = CameraDevice.Instance.SetFrameFormat(Image.PIXEL_FORMAT.RGB888, true);
+        var isFrameFormatSet = CameraDevice.Instance.SetFrameFormat(Image.PIXEL_FORMAT.GRAYSCALE, true);
+        Debug.Log(String.Format("FormatSet : {0}", isFrameFormatSet));
+
+		// Force autofocus.
+		var isAutoFocus = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		if (!isAutoFocus)
+		{
+			CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_NORMAL);
+		}
+		Debug.Log(String.Format("AutoFocus : {0}", isAutoFocus));
+		cameraInitialized = true;
+        Debug.Log("cameraInitialized set to true");
+	}
+
+	private void Update()
+	{
+        if (Input.GetKeyDown("k"))
+        {
+            mainMenu.spawnPatientUI();
+            GameObject camera = GameObject.FindGameObjectWithTag("AR Camera");
+            Destroy(camera);
+            Destroy(gameObject);
+        }
+
+		if (cameraInitialized)
+		{
+            
+            try
+            {
+                
+				var cameraFeed = CameraDevice.Instance.GetCameraImage(Image.PIXEL_FORMAT.GRAYSCALE);
+				if (cameraFeed == null)
+				{
+                    Debug.Log("No camera device");
+					return;
+				}
+                Debug.Log("Trying to scan");
+                var data = barCodeReader.Decode(cameraFeed.Pixels, cameraFeed.BufferWidth, cameraFeed.BufferHeight, RGBLuminanceSource.BitmapFormat.Gray8);
+				if (data != null)
+				{
+					// QRCode detected.
+					Debug.Log(data.Text);
+                    mainMenu.spawnPatientUI();
+                    GameObject camera = GameObject.FindGameObjectWithTag("AR Camera");
+                    Destroy(camera);
+                    Destroy(gameObject);
+
+                }
+				else
+				{
+					Debug.Log("No QR code detected!");
+				}
+			}
+			catch (Exception e)
+			{
+                Debug.Log("failing to scan");
+                Debug.LogError(e.Message);
+			}
+		}
+	}
+}
