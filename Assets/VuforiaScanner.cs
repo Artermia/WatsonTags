ac71c69659be5b32f0b29e3906169585cff11db3
David <lnma@umich.edu>
2017-04-06 11:24:16 -0400

    vuforia scanner clean

--- Assets/VuforiaScanner.cs
+++ Assets/VuforiaScanner.cs
@@ -1,15 +1,9 @@
-﻿
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-
 using System;
 using System.Collections;
-
 using Vuforia;
-
 using System.Threading;
-
 
 [AddComponentMenu("System/VuforiaScanner")]
 public class VuforiaScanner : ImageTargetAbstractBehaviour, ITrackableEventHandler
@@ -17,7 +11,6 @@ public class VuforiaScanner : ImageTargetAbstractBehaviour, ITrackableEventHandl
     private TrackableBehaviour mTrackableBehaviour;
     private bool cameraInitialized;
     public UISpawn mainMenu;
-    public GameObject QRcamera;
 
 	void Start()
 	{
@@ -26,29 +19,8 @@ public class VuforiaScanner : ImageTargetAbstractBehaviour, ITrackableEventHandl
         {
             mTrackableBehaviour.RegisterTrackableEventHandler(this);
         }
-        StartCoroutine(InitializeCamera());
 	}
 
-	private IEnumerator InitializeCamera()
-	{
-		// Waiting a little seem to avoid the Vuforia's crashes.
-		yield return new WaitForSeconds(1.25f);
-
-        //var isFrameFormatSet = CameraDevice.Instance.SetFrameFormat(Image.PIXEL_FORMAT.RGB888, true);
-        var isFrameFormatSet = CameraDevice.Instance.SetFrameFormat(Vuforia.Image.PIXEL_FORMAT.GRAYSCALE, true);
-        Debug.Log(String.Format("FormatSet : {0}", isFrameFormatSet));
-
-		// Force autofocus.
-		var isAutoFocus = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
-		if (!isAutoFocus)
-		{
-			CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_NORMAL);
-		}
-		Debug.Log(String.Format("AutoFocus : {0}", isAutoFocus));
-		cameraInitialized = true;
-        Debug.Log("cameraInitialized set to true");
-        
-    }
     public void OnTrackableStateChanged(
                                         TrackableBehaviour.Status previousStatus,
                                         TrackableBehaviour.Status newStatus)
@@ -69,7 +41,6 @@ public class VuforiaScanner : ImageTargetAbstractBehaviour, ITrackableEventHandl
             //target is lost
         }
     }
-
 
     private void Update()
 	{
